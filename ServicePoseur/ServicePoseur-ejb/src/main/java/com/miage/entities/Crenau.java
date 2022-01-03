0c8c96764f19f7f48544e0598bdb36848d579e6f
julienrbd <jrobardet81@gmail.com>
2022-01-03 22:04:08 +0100

    entites

--- ServicePoseur/ServicePoseur-ejb/src/main/java/com/miage/entities/Crenau.java
+++ ServicePoseur/ServicePoseur-ejb/src/main/java/com/miage/entities/Crenau.java
@@ -6,10 +6,12 @@
 package com.miage.entities;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -18,67 +20,70 @@ import javax.persistence.Id;
 @Entity
 public class Crenau implements Serializable {
 
-        private static final long serialVersionUID = 1L;
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long id;
-        private String debut;
-        private String fin ;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Calendar debut;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Calendar fin;
 
-        public Crenau(Long id, String debut, String fin) {
-                this.id = id;
-                this.debut = debut;
-                this.fin = fin;
-        }
-        
+    public Crenau() {
+    }
 
-        public String getDebut() {
-                return debut;
-        }
+    public Crenau(Calendar debut, Calendar fin) {
+        this.debut = debut;
+        this.fin = fin;
+    }
 
-        public void setDebut(String debut) {
-                this.debut = debut;
-        }
+    public Calendar getDebut() {
+        return debut;
+    }
 
-        public String getFin() {
-                return fin;
-        }
+    public void setDebut(Calendar debut) {
+        this.debut = debut;
+    }
 
-        public void setFin(String fin) {
-                this.fin = fin;
-        }
-        
-        public Long getId() {
-                return id;
-        }
+    public Calendar getFin() {
+        return fin;
+    }
 
-        public void setId(Long id) {
-                this.id = id;
-        }
+    public void setFin(Calendar fin) {
+        this.fin = fin;
+    }
 
-        @Override
-        public int hashCode() {
-                int hash = 0;
-                hash += (id != null ? id.hashCode() : 0);
-                return hash;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        @Override
-        public boolean equals(Object object) {
-                // TODO: Warning - this method won't work in the case the id fields are not set
-                if (!(object instanceof Crenau)) {
-                        return false;
-                }
-                Crenau other = (Crenau) object;
-                if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-                        return false;
-                }
-                return true;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        @Override
-        public String toString() {
-                return "com.miage.Facade.entities.Crenau[ id=" + id + " ]";
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Crenau)) {
+            return false;
         }
-        
+        Crenau other = (Crenau) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.miage.Facade.entities.Crenau[ id=" + id + " ]";
+    }
+
 }
--- ServicePoseur/ServicePoseur-ejb/src/main/java/com/miage/entities/Poseur.java
+++ ServicePoseur/ServicePoseur-ejb/src/main/java/com/miage/entities/Poseur.java
@@ -6,10 +6,12 @@
 package com.miage.entities;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -18,79 +20,78 @@ import javax.persistence.Id;
 @Entity
 public class Poseur implements Serializable {
 
-        private static final long serialVersionUID = 1L;
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long id;
-        private String créneau;
-        private String[] affaires;
-        private String statut;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private String prenom;
+    @OneToMany
+    private List<Crenau> Creneau;
 
-        public String getStatut() {
-                return statut;
-        }
+    public List<Crenau> getCreneau() {
+        return Creneau;
+    }
 
-        public void setStatut(String statut) {
-                this.statut = statut;
-        }
+    public void setCreneau(List<Crenau> Creneau) {
+        this.Creneau = Creneau;
+    }
 
-        public Poseur(Long id, String créneau, String[] affaires) {
-                this.id = id;
-                this.créneau = créneau;
-                this.affaires = affaires;
-        }
-      
+    public String getNom() {
+        return nom;
+    }
 
-        public String[] getAffaires() {
-                return affaires;
-        }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-        public void setAffaires(String[] affaires) {
-                this.affaires = affaires;
-        }
-        
-        
-        public String getCréneau() {
-                return créneau;
-        }
+    public String getPrenom() {
+        return prenom;
+    }
 
-        public void setCréneau(String créneau) {
-                this.créneau = créneau;
-        }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
-        
-        
-        public Long getId() {
-                return id;
-        }
+    public Long getId() {
+        return id;
+    }
 
-        public void setId(Long id) {
-                this.id = id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        @Override
-        public int hashCode() {
-                int hash = 0;
-                hash += (id != null ? id.hashCode() : 0);
-                return hash;
-        }
+    public Poseur() {
+    }
 
-        @Override
-        public boolean equals(Object object) {
-                // TODO: Warning - this method won't work in the case the id fields are not set
-                if (!(object instanceof Poseur)) {
-                        return false;
-                }
-                Poseur other = (Poseur) object;
-                if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-                        return false;
-                }
-                return true;
-        }
+    public Poseur(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
 
-        @Override
-        public String toString() {
-                return "com.miage.Poseur[ id=" + id + " ]";
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Poseur)) {
+            return false;
         }
-        
+        Poseur other = (Poseur) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.miage.Poseur[ id=" + id + " ]";
+    }
+
 }
